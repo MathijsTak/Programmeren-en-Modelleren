0a35d0678c629d0c54de28b9ac463cc264c8876a
MathijsTak <mathijs.tak@outlook.com>
2022-11-16 12:02:52 +0100

    Added verKdag assignment

---
+++ verKdag/verKdag/verKdag.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+vandaag = datetime.now().strftime("%d-%m-%Y")
+vandaag = datetime.strptime(vandaag, "%d-%m-%Y")
+verKdag = None
+leeftijd = 0
+
+# Vraag de user om zijn/haar verjaardag en zet dit om naar aparte integers
+verjaardag = input("Wanneer ben je jarig? (dd-mm-YYYY)\n")
+
+verjaardag = datetime.strptime(verjaardag, "%d-%m-%Y")
+
+# Totdat er een verKdag later dan vandaag of vandaag is gevonden voeg 1000 toe aan de verjaardag
+while verKdag == None:
+    leeftijd += 1
+    datum = verjaardag + timedelta(days=leeftijd * 1000)
+    if datum > vandaag:
+        verschil = datum - vandaag
+        verschil = verschil.days
+        datum = datum.strftime("%d-%m-%Y")
+        verKdag = f"over {verschil} dagen, op {datum}"
+    if datum == vandaag:
+        verKdag = "vandaag"
+
+
+
+# Output is de verKdagen en de volgende verKdag
+print(f"Je hebt al {leeftijd} verKdag(en) gehad en je volgende verKdag is {verKdag}.")
